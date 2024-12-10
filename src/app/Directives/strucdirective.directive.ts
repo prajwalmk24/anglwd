8d4be1861c75aca37d7e9f328d9d4ee6221e2bb6
prajwalmk24 <mkprajwal65@gmail.com>
2024-12-10 19:26:39 +0530

    custime directive

--- src/app/Directives/strucdirective.directive.ts
+++ src/app/Directives/strucdirective.directive.ts
@@ -1,13 +1,13 @@
 import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: 'appStrucdirective',
+  selector: '[appStrucdirective]',
   standalone: true
 })
 export class StrucdirectiveDirective implements OnInit {
 
 
-  @Input() customIf:boolean= true;
+  @Input() appStrucdirective:boolean= true;
 
   constructor(
     private tempRef:TemplateRef<any>,
@@ -15,7 +15,7 @@ export class StrucdirectiveDirective implements OnInit {
   ) { }
 
   ngOnInit(): void {
-      if(this.customIf){
+      if(this.appStrucdirective){
         this.viewContRef.createEmbeddedView(this.tempRef);
       }
       else{
